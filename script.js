062792c83cda7eb4d929be401aaa36d5cfe8c82b
Daniel  nezuko <51461877+Daniel-nezuko@users.noreply.github.com>
2020-02-24 17:31:44 -0300

    Add files via upload
    todos os arquivos do site

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('header button').addEventListener('click', function() {
+    document.querySelector('.form').classList.toggle('hide')
+});
+document.querySelector('footer button').addEventListener('click', function(){
+    document.querySelector('.sobre').classList.toggle('hide')
+})
