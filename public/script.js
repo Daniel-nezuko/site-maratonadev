47344624a7a150628572df907ae0c98e75269de2
Daniel  nezuko <51461877+Daniel-nezuko@users.noreply.github.com>
2020-03-12 09:49:47 -0300

    script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
 
+document.querySelector('header button').addEventListener('click', function() {
+    document.querySelector('.form').classList.toggle('hide')
+});
+document.querySelector('footer button').addEventListener('click', function(){
+    document.querySelector('.sobre').classList.toggle('hide')
+})
