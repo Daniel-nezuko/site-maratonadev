e076a8580713d032dd812f063324c56aae43bcf0
Daniel  nezuko <51461877+Daniel-nezuko@users.noreply.github.com>
2020-03-12 09:38:15 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('header button').addEventListener('click', function() {
-    document.querySelector('.form').classList.toggle('hide')
-});
-document.querySelector('footer button').addEventListener('click', function(){
-    document.querySelector('.sobre').classList.toggle('hide')
-})
